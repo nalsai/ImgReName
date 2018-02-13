2c2f99f6f378707d7ee78c09ed0e4e599f24317a
Nalsai <Nalsai@users.noreply.github.com>
2018-02-13 12:25:13 +0100

    update

--- ImgReName/obj/Debug/ImgReName_Content.g.i.cs
+++ ImgReName/obj/Debug/ImgReName_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("exotiska%20djur.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("imgrenamei.ico")]
 
 
