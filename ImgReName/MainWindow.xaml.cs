cafb225e3b1b8de8ca7b2ddc23ea96a0583bb74e
nalsai <contact@nalsai.de>
2020-07-10 18:52:32 +0200

    update packages

--- ImgReName/MainWindow.xaml.cs
+++ ImgReName/MainWindow.xaml.cs
@@ -121,7 +121,7 @@ namespace ImgReName
                         }
                     }
 
-                ChooseName:
+                    ChooseName:
 
                     string newname = "";
 
